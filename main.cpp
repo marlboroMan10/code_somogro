40b4e4b7123c1fffd2b293a02fde35890a17f198
Anubhab <142051923+marlboroMan10@users.noreply.github.com>
2024-07-18 14:58:10 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll 
+
+void solve(){
+
+
+}
+
+
+int main(){
+
+    int t; cin>>t;
+
+    while(t--){
+        int n,m; cin>>n>>m;
+        vector<int> nums(n);
+
+        for(auto& num:nums) cin>>num;
+
+        cout<<solve()<<endl;
+
+    }
+
+
+return 0;
+}
